c70e5c44c640dfed3139df42a30688c7053011c2
Martin Vician <info@vician.cz>
2016-02-29 19:39:50 +0100

    fixed: init, dependencies, ...

--- init.sh
+++ init.sh
@@ -14,17 +14,16 @@ SCRIPT="$base_dir/yubilock.sh"
 if [ -f $udev_file ]; then
 	echo "Udev file already exist. If you want to reinit, please remove it first."
 	echo "sudo rm $udev_file"
-	exit 0
-fi
+else
+	# Create the udev file
+	echo -e 'SUBSYSTEM=="usb", ACTION=="remove", ENV{ID_VENDOR_ID}=="'$ID_VENDOR_ID'", ENV{ID_MODEL_ID}="'$ID_MODEL_ID'", RUN+="'$SCRIPT' enable"\nSUBSYSTEM=="usb", ACTION=="add", ENV{ID_VENDOR_ID}=="'$ID_VENDOR_ID'", ENV{ID_MODEL_ID}="'$ID_MODEL_ID'", RUN+="'$SCRIPT' disable"' | sudo tee $udev_file
 
-# Create the udev file
-echo -e 'SUBSYSTEM=="usb", ACTION=="remove", ENV{ID_VENDOR_ID}=="'$ID_VENDOR_ID'", ENV{ID_MODEL_ID}="'$ID_MODEL_ID'", RUN+="'$SCRIPT' enable\nSUBSYSTEM=="usb", ACTION=="add", ENV{ID_VENDOR_ID}=="'$ID_VENDOR_ID'", ENV{ID_MODEL_ID}="'$ID_MODEL_ID'", RUN+="'$SCRIPT' disable"' > $udev_file
-
-# Reload the udevadm
-sudo udevadm control --reload-rules
-if [ $? -ne 0 ]; then
-	echo "ERROR: Cannot reload udevadm!"
-	exit 1
+	# Reload the udevadm
+	sudo udevadm control --reload-rules
+	if [ $? -ne 0 ]; then
+		echo "ERROR: Cannot reload udevadm!"
+		exit 1
+	fi
 fi
 
 # Does the trusted file exist?
@@ -32,13 +31,21 @@ if [ -f $trusted_file ]; then
 	echo "Trusted file already exist. If you want to reinit, please remove it first."
 	echo "rm $trusted_file"
 	exit 0
-fi
+else
+	which ykinfo 1>/dev/null 1>/dev/null
+	if [ $? -ne 0 ]; then
+		echo "Please install yubikey-personalization first."
+		echo "sudo aptitude install yubikey-personalization"
+		echo "or"
+		echo "sudo dnf install yubikey-personalization"
+		exit 0
+	fi
 
-# Create the trusted file
-echo "$USER:$(ykinfo -q -s)" > $trusted_file
-if [ $? -ne 0 ]; then
-	echo "ERROR: Cannot create trusted file"
-	exit 0
+	# Create the trusted file
+	echo "$USER:$(ykinfo -q -s)" > $trusted_file
+	if [ $? -ne 0 ]; then
+		echo "ERROR: Cannot create trusted file"
+		exit 0
+	fi
 fi
-
 
