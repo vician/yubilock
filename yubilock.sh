b9a7f7035a9f9b2aeba901d3329fae49bffb08c5
Martin Vician <martin.vician@nic.cz>
2016-04-28 11:20:44 +0200

    verbose and logging

--- yubilock.sh
+++ yubilock.sh
@@ -2,10 +2,15 @@
 
 # Based on: https://www.dalemacartney.com/2013/01/14/locking-and-unlocking-the-gnome3-session-with-a-yubikey/
 
+user=`ps aux | grep -v root | grep session | head -n 1 | awk '{print $1}'`
+
+#set -x
+#exec >> /home/$user/Documents/src/yubilock/yubilock.log 2>&1
+#echo date $1
+
 base_dir=$(dirname $0)
 base_dir=$(realpath $base_dir)
 trusted="$base_dir/trusted"
-user=`ps aux | grep -v root | grep session | head -n 1 | awk '{print $1}'`
 
 do_lock="/home/$user/.i3/lock.sh force"
 do_unlock="pkill i3lock"
@@ -16,8 +21,9 @@ case "$1" in
 		su $user -c "DISPLAY=:0 $do_lock"
 	;;
 	disable)
-		echo "try unlocking"
+		echo "check unlocking"
 		if [ -n ${user} -a "$(grep -c ${user}:$(ykinfo -q -s) ${trusted})" == "1" ]; then
+			echo "trused -> unlock"
 			$do_unlock
 		fi
 	;;
